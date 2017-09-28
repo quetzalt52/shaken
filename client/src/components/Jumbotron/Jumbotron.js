98f4eaa16c3452c7af06660cc4204941f63a85b0
quetzalt52 <lalys_espinoza@yahoo.com>
2017-09-28 16:42:50 -0700

    fixed my own bugs and now it reflects Sals work... Sorry

--- client/src/components/Jumbotron/Jumbotron.js
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -6,17 +6,11 @@ import background from "../../assets/img/martini-image.jpg"
 const Jumbotron = () => (
   <div className="jumbotron text-center">
     <img src={background} className="backgroundImage" alt="martini" />
-    <h1>Martini-Shaken</h1>
-
-const Jumbotron2 = () => (
-  <div className="jumbotron text-center">
-    <h1>The Martini Project</h1>
-
-    <a
-      target="_blank"
-      rel="noopener noreferrer"
-      href="http://www.recipepuppy.com/about/api/"
-    >
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="http://addb.absolutdrinks.com/drinks/"
+      >
          Powered by Absolut Drinks
     </a>
   </div>
