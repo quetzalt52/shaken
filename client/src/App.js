4c709a94a53eb48361e69369f1752301d2874315
quetzalt52 <lalys_espinoza@yahoo.com>
2017-09-28 13:14:48 -0700

    made same changes on server file

--- client/src/App.js
+++ client/src/App.js
@@ -87,14 +87,14 @@ class App extends Component {
           </Row>
         </Container>
 
-//       <div className="App">
-//         <div className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <h2>Welcome to The Martini Project</h2>
-//         </div>
-//         <p className="App-intro">
-//           To get started, edit <code>src/App.js</code> and save to reload.
-//         </p>
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to The Martini Project</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
 
       </div>
     );
