6eb6d52d38f49a5d5ad65987566be3e77821339b
Albert Factora <albert.factora@gmail.com>
2017-09-30 19:33:58 -0700

    added various models

--- models/index.js
+++ models/index.js
@@ -1,3 +1,8 @@
 module.exports = {
-    Recipe: require("./recipe")
+    Ingredient: require("./Ingredient"),
+    Recipe: require("./Recipe"),
+    Unit: require("./Unit"),
+    User: require("./User")
+//foo
   };
+
