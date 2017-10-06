05ff532817e2de5f83ef7093c4af3cfbaf8e2320
quetzalt52 <lalys_espinoza@yahoo.com>
2017-10-06 11:13:46 -0700

    final version

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,20 +1,3 @@
-// import axios from "axios";
-// // Include the Axios library for HTTP requests
-
-// const BaseURL ="http://addb.absolutdrinks.com/drinks/";
-// //  API Key
-// const APIKey = "de7934f82114431d8c7a617a36bb36de";
-
-
-// // // The getRecipes method retrieves recipes from the server
-// // // It accepts a "query" or term to search the recipe api for
-// export default {
-//   getRecipes: function(query) {
-//     return axios.get(BaseURL+query+"/?apiKey="+APIKey);
-//     //http://addb.absolutdrinks.com/drinks/blue-moon/?apiKey=de7934f82114431d8c7a617a36bb36de
-//   }
-// };
-
 import axios from 'axios';
 
 export default {
