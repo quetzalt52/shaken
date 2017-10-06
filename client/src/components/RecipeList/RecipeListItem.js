710f4a9d07b4b12314ae6e7d359fca6c00b12a18
salmui_ <salhuang22@gmail.com>
2017-10-05 23:28:25 -0700

    deleted the thumbnail placeholder

--- client/src/components/RecipeList/RecipeListItem.js
+++ client/src/components/RecipeList/RecipeListItem.js
@@ -7,9 +7,6 @@ export const RecipeListItem = props => (
   <li className="list-group-item">
     <Container>
       <Row>
-        <Col size="xs-4 sm-2">
-          <Thumbnail src={props.thumbnail || "https://placehold.it/300x300"} />
-        </Col>
         <Col size="xs-8 sm-9">
           <h3>{props.title}</h3>
           <p>Ingredients: {props.ingredients.map(ingredient => (
